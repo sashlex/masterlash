dd5af82ecf97ade4b2567d102f2b0023354a430a
sashlex <sashlex@yandex.ru>
2017-05-10 14:14:59 +0300

    First commit.

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// 'use strict';
+
+// var webpack = require( 'webpack' );
+
+// module.exports = {
+//    watch: false,
+//    context: __dirname + '/www/public',
+//    entry: {
+//       index: 'js/script',
+//       css: 'css/css'
+
+//    },
+//    output: {
+//       path: __dirname + '/www/public',
+//       filename: '[name].js'
+//    },
+//    resolve: {
+//       extensions: [ '', '.js', '.styl' ]
+//    },
+//    module: {
+//       loaders: [
+//          {
+//             loader: 'traceur?experimental&runtime'
+//          }, {
+//             test: /\.styl$/,
+//             loader: 'style!css!autoprefixer?browsers=last 2 versions'
+//          }
+//       ]
+//    }
+// };
