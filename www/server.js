812d9ae248b47c4c90ae598f676d56e611abf50a
sashlex <sashlex@yandex.ru>
2017-05-10 17:25:20 +0300

    Add logger.

--- www/server.js
+++ www/server.js
@@ -1,9 +1,24 @@
 'use strict';
 
-const koa = require( 'koa' );
-const app = new koa();
+const winston = require( 'winston' );
+const koaLogger = require( 'koa-logger-winston' );
+const Koa = require( 'koa' );
+const app = new Koa();
 
-app.use( ctx => {
+/* logger setup */
+const loggerOptions = {
+   level: 'silly',
+   colorize: true,
+   filename: 'log/access.log',
+   maxsize: 102400,
+   maxFiles: 12,
+   tailable: true
+};
+winston.add( winston.transports.File, loggerOptions );
+app.use( koaLogger( winston ) );
+
+app.use( async ( ctx, next ) => {
+   await next();
    ctx.body = 'Hello World';
 });
 
